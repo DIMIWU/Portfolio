ed1f947df11435422fac1e2a984062ca10bf39a6
Dimi Wu <565105022@qq.com>
2022-05-07 12:52:14 +0800

    1
    1

--- javascript/compress.js
+++ javascript/compress.js
@@ -1,13 +1,20 @@
 <script>
-$(function(){
-	//一段正则，匹配所有_min.的图片src属性
-	var test = /_min\./
-	//遍历所有的图片节点
-	$("img").each(function(index,obj){	
-		if(test.test($(this).attr("src"))){
-			var reSrc = $(this).attr("src").replace(test,".");
-			$(this).attr("src",reSrc)
-		}		
-	})
-})
+  window.onload = () => {
+    const imgs = document.getElementsByTagName("img")
+    let n = 0
+    const imgLazyLoading = () => {
+      const seeHeight = window.innerHeight
+      const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+      for (let i=n; i< imgs.length; i++) {
+        if (imgs[i].offsetTop < seeHeight + scrollTop) {
+          if (imgs[i].getAttribute('src') === 'images/loading.gif') {
+            imgs[i].src = imgs[i].getAttribute('data-src')
+          }
+          n = i + 1
+        }
+      }
+    }
+    window.addEventListener("scroll", imgLazyLoading)
+    imgLazyLoading()
+  }
 </script>
